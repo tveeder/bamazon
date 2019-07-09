e4c9e16d7f3d089283cb40ff593cf2c330cd9ccb
Other <Other@TRVs-MacBook-Air.local>
2019-07-08 23:36:51 -0500

    commiting with prompts working

--- bamazon.sql
+++ bamazon.sql
@@ -1,5 +1,3 @@
-
-
 DROP DATABASE IF EXISTS bamazonDB;
 
 CREATE DATABASE bamazonDB;
@@ -8,45 +6,60 @@ USE bamazonDB;
 
 CREATE TABLE products
 (
-  id INT NOT NULL
+
+  item_id INT (20) NOT NULL
   AUTO_INCREMENT,
-  item_id VARCHAR
-  (45) NOT NULL,
   product_name VARCHAR
   (45) NULL,
   department_name VARCHAR
   (45) NOT NULL,
   price DECIMAL
-  (x,2) NOT NULL, 
+  (10,2) NOT NULL, 
   stock_quantity INT
   (45),
   PRIMARY KEY
-  (id)
+  (item_id)
+
 );
 
+  INSERT INTO products
+    (item_id, product_name, department_name, price, stock_quantity)
+  VALUES
+    ("123", "watch", "luxury", 12.34, 6868),
+    ("111", "shoes", "football", 11.23, 1111),
+    ("222", "pants", "dance", 22.55, 2222),
+    ("333", "helmut", "hockey", 33.88, 3333),
+    ("444", "club", "golf", 44.66, 4444),
+    ("555", "timer", "track", 55.44, 5555),
+    ("666", "mask", "goalie", 66.21, 6666),
+    ("777", "shorts", "tennis", 77.99, 7777),
+    ("888", "wristband", "running,", 88.99, 8888),
+    ("999", "ribbon", "racing", 10.10, 9999);
 
-
-  Select *
+  SELECT *
   FROM products;
 
-  INSERT INTO products
-    (item_id, product_name, department_name, price, stock_quantity)
-  VALUES
-    (110, "sweatbands", "soccer", 10.55, 01),
-    (210, "gloves", "basketball", 20.25, 22),
-    (330, "gatorade", "football", 30.55, 33),
-    (440, "jersey", "hockey", 40.44, 44),
-    (550, "kleats", "football", 50.55, 55),
-    (600, "pants", "soccer", 60.55, 66),
-    (725, "bats", "baseball", 70.25, 77),
-    (850, "clubs", "baseball", 80.45, 88),
-    (905, "footballs", "hockey", 90.45, 99),
-    (1010, "hi-tops", "basketball", 100.99, 10)
+  CREATE TABLE Departments
+  (
+    DepartmentID INT
+    AUTO_INCREMENT NOT NULL,
+    DepartmentName VARCHAR
+    (50) NOT NULL,
+    OverHeadCosts DECIMAL
+    (10,2) NOT NULL,
+    TotalSales DECIMAL
+    (10,2) NOT NULL,
+    PRIMARY KEY
+    (DepartmentID));
 
-
-
-  INSERT INTO products
-    (item_id, product_name, department_name, price, stock_quantity)
-
-  VALUES
-    ("123", "watch", "luxury", 12.34, 6868)  
+    INSERT INTO Departments
+      (DepartmentName, OverHeadCosts, TotalSales)
+    VALUES
+      ('a', 50000.00, 15000.00),
+      ('b', 20000.00, 12000.00),
+      ('c', 30000.00, 15000.00),
+      ('d & e', 3000.00, 12000.00),
+      ('f', 1200.00, 15000.00),
+      ('g', 40000.00, 12000.00),
+      ('h', 35000.00, 15000.00),
+      ('j', 12000.00, 12000.00);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
     user: "root",
 
     // Your password
-    password: "",
+    password: "Ilgeneric12!",
     database: "bamazonDB"
 });
 
@@ -20,8 +20,129 @@ connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     // afterConnection();
-    // start();
+    start();
 });
+
+function start() {
+
+    console.log('function start() was called')
+    console.log('list everything that is for sale')
+
+    //prints the items for sale and their details
+    //res is your object
+    connection.query('SELECT * FROM products', function (err, res) {
+
+        console.log('connection query was called')
+        if (err) throw err;
+
+        console.log('BAMAZON BAMAZON BAMAZON')
+
+        for (var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].item_id + " | " + "Name: " + res[i].product_name + " | " + "dept: " + res[i].department_name + " | " + "price: " + res[i].price + " | " + "stock quantity: " + res[i].stock_quantity);
+            console.log('+++++++++++++++++++++++++++++++++++++')
+        }
+
+        //     })
+        // }
+
+
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "id",
+                message: "What is the ID of the product you would like to purchase?",
+                validate: function (value) {
+                    console.log(value);
+                    console.log('test one')
+                    if (isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: "input",
+                name: "qty",
+                message: "How much would you like to purchase?",
+
+                validate: function (value) {
+                    if (isNaN(value)) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            },
+        ]).then(function (answer) {
+            console.log('test two')
+            console.log(answer)
+            console.log(answer.id)
+            console.log(answer.qty)
+            var whatToBuy = (answer.id) - 1;   // this is going to decrement the item
+            var howMuchToBuy = parseInt(answer.qty);
+            var grandTotal = parseFloat(((res[whatToBuy].Price) * howMuchToBuy).toFixed(2));
+
+            // checks to see if the supply of the item sufficient?
+            if (res[whatToBuy].stock_quantity >= howMuchToBuy) {
+                //after purchase, updates quantity in products
+                connection.query("UPDATE products SET ? WHERE ?", [
+                    { stock_quantity: (res[whatToBuy].stock_quantity - howMuchToBuy) },
+                    { item_id: answer.id }
+                ], function (err, result) {
+                    if (err) throw err;
+                    console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". A Bamazon Drone will deliever your items");
+                });
+
+
+                console.log('testing for departments ')
+                connection.query("SELECT * FROM Departments", function (err, deptRes) {
+                    console.log(deptRes)
+                    if (err) throw err;
+                    var index;
+                    console.log(index)
+                    console.log(deptRes.length)
+                    for (var i = 0; i < deptRes.length; i++) {
+                        if (deptRes[i].DepartmentName === res[whatToBuy].DepartmentName) {
+                            index = i;
+                        }
+                    }
+
+                    //updates totalSales in departments table
+
+                    console.log('testing for total sales ')
+                    connection.query("UPDATE Departments SET ? WHERE ?", [
+                        { TotalSales: deptRes[index].TotalSales + grandTotal },
+                        { DepartmentName: res[whatToBuy].DepartmentName }
+                    ], function (err, deptRes) {
+                        if (err) throw err;
+                        //console.log("Updated Dept Sales.");
+
+                    });
+                });
+
+            } else {
+                console.log("Sorry, there's not enough in stock!");
+            }
+
+        })
+
+    })
+
+}
+
+
+
+// console.log("\nID: 55444 " + " Department: sporting goods " + " Price: 8.95 " + " QTY: 65 ");
+// console.log('+++++++++++++++++++++++++++++++++++++')
+// console.log("\nID: 877 " + " Department: hockey " + " Price: 11.35 " + " QTY: 99 ");
+// console.log('+++++++++++++++++++++++++++++++++++++')
+// console.log("\nID: 33333 " + " Department: baseball " + " Price: 21.33 " + " QTY: 33 ");
+// console.log('+++++++++++++++++++++++++++++++++++++')
+// console.log("\nID: 3434 " + " Department: sporting goods " + " Price: 23.5 " + " QTY: 625 ");
+// console.log('+++++++++++++++++++++++++++++++++++++')
+
 
 // worked with tutor to connect to database
 //  connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
@@ -41,81 +162,3 @@ connection.connect(function (err) {
 // use mysql;
 // update user set authentication_string = PASSWORD('') where User = 'root';
 // flush privileges;
-
-function start() {
-
-    console.log('function start() was called')
-    console.log('list everything that is for sale')
-
-    //prints the items for sale and their details
-    connection.query('SELECT * FROM products', function (err, res) {
-
-        console.log('connection query was called')
-        if (err) throw err;
-
-        console.log('BAMAZON BAMAZON BAMAZON')
-
-        for (var i = 0; i < res.length; i++) {
-            console.log("ID: " + res[i].ItemID + " | " + "Product: " + res[i].ProductName + " | " + "Department: " + res[i].DepartmentName + " | " + "Price: " + res[i].Price + " | " + "QTY: " + res[i].StockQuantity);
-            console.log('+++++++++++++++++++++++++++++++++++++')
-        }
-
-    })
-}
-
-start();
-
-inquirer.prompt([
-    {
-        type: "input",
-        name: "id",
-        message: "What is the ID of the product you would like to purchase?",
-        validate: function (value) {
-            if (isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    },
-
-    {
-        type: "input",
-        name: "qty",
-        message: "How much would you like to purchase?",
-
-        validate: function (value) {
-            if (isNaN(value)) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-    },
-]).then(function (answer) {
-    var whatToBuy = (answer.id) - 1;   // this is going to decrement the item
-    var howMuchToBuy = parseInt(answer.qty);
-    var grandTotal = parseFloat(((res[whatToBuy].Price) * howMuchToBuy).toFixed(2));
-
-    // is the supply of the item sufficient????????????
-    if (res[whatToBuy].StockQuantity >= howMuchToBuy) {
-        //after purchase, updates quantity in Products
-        connection.query("UPDATE Products SET ? WHERE ?", [
-            { StockQuantity: (res[whatToBuy].StockQuantity - howMuchToBuy) },
-            { ItemID: answer.id }
-        ], function (err, result) {
-            if (err) throw err;
-            console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". A Bamazon Drone will deliever your items");
-        });
-    };
-});
-
-
-console.log("\nID: 55444 " + " Department: sporting goods " + " Price: 8.95 " + " QTY: 65 ");
-console.log('+++++++++++++++++++++++++++++++++++++')
-console.log("\nID: 877 " + " Department: hockey " + " Price: 11.35 " + " QTY: 99 ");
-console.log('+++++++++++++++++++++++++++++++++++++')
-console.log("\nID: 33333 " + " Department: baseball " + " Price: 21.33 " + " QTY: 33 ");
-console.log('+++++++++++++++++++++++++++++++++++++')
-console.log("\nID: 3434 " + " Department: sporting goods " + " Price: 23.5 " + " QTY: 625 ");
-console.log('+++++++++++++++++++++++++++++++++++++')
